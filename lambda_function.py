cf95f32a073f191df1d67991c0f467817dd29d3f
root <root@ip-172-31-39-243.ec2.internal>
2019-03-17 19:31:40 +0000

    initial commit

---
+++ lambda_function.py
@@ -0,0 +1,36 @@
+import boto3
+from botocore.vendored import requests
+
+def lambda_handler(event):
+    client = boto3.client('dynamodb')
+    try:
+        url = str(event['url'])
+    except Exception:
+        return {"message": "missing url in input !"}
+    try:
+        word = str(event['word'])
+    except Exception:
+        return {"message": "missing word in input !"}
+    try:
+        r = requests.get(url)
+        status = r.status_code
+        if status == 200:
+            count = r.text.count(word)
+            response = {"status": status, "count": count}
+            # Post to dynamo db
+            item_counter = {
+                "count": {"N": str(count)},
+                "url": {"S": url},
+                "word": {"S": word},
+                "status": {"N": str(status)}
+                }
+            client.put_item(
+                    TableName='Word_Counter',
+                    Item=item_counter
+                    )
+            return response
+        else:
+            response = {"status": status, "message": "error in url"}
+            return response
+    except Exception:
+        return {"message": "error in url to fetch !"}
